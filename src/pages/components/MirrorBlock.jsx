57accf5db3c1acdb451be69937bd3c0e3cbc6543
ali <ahmadshahzadrajpoot005@gmail.com>
2025-10-27 15:03:12 +0500

    changes completed

--- src/pages/components/MirrorBlock.jsx
+++ src/pages/components/MirrorBlock.jsx
@@ -8,36 +8,29 @@ const MirrorBlock = () => {
       <div className="flex flex-col lg:flex-row justify-center items-start gap-6 ">
         {/* Left side */}
         <div className="flex flex-col gap-6 px-3 md:px-0">
-          <img src="/images/Card5.png" alt="" className="rounded-2xl w-full max-w-sm object-cover" />
-          <img src="/images/Card6.png" alt="" className="rounded-2xl w-full max-w-sm object-cover" />
-          <img src="/images/Card4.png" alt="" className="rounded-2xl w-full max-w-sm object-cover" />
+          <img src="/images/Card5.png" alt="" className="rounded-2xl w-full max-w-lg object-cover" />
+          <img src="/images/Card6.png" alt="" className="rounded-2xl w-full max-w-lg object-cover" />
+          <img src="/images/Card4.png" alt="" className="rounded-2xl w-full max-w-lg object-cover" />
         </div>
 
         {/* Right side */}
         <div className="flex flex-col gap-6">
-          <img src="/images/Card3.png" alt="" className="rounded-2xl w-full max-w-sm object-cover px-3 md:px-0" />
-          <div className="flex gap-6 justify-between flex-col items-center md:flex-row px-3 md:px-0">
-            <img src="/images/Card5.png" alt="" className="rounded-2xl w-full max-w-sm object-cover block sm:hidden" />
-            <img src="/images/Card6.png" alt="" className="rounded-2xl w-full max-w-sm object-cover  block sm:hidden" />
-            <img
-              src="/images/Card.png"
-              alt=""
-              className="rounded-2xl w-full h-[370px] max-w-[250px] hidden sm:block object-cover"
-            />
-            <img
-              src="/images/Card2.png"
-              alt=""
-              className="rounded-2xl w-full h-[370px] max-w-[250px] object-cover hidden sm:block"
-            />
+          <img src="/images/Card3.png" alt="" className="rounded-2xl w-full max-w-lg object-cover px-3 md:px-0" />
+          <div className="gap-6 justify-between items-center px-3 md:px-0">
+            {/* <img src="/images/Card5.png" alt="" className="rounded-2xl w-full max-w-lg object-cover block sm:hidden" />
+        <img src="/images/Card6.png" alt="" className="rounded-2xl w-full max-w-lg object-cover  block sm:hidden" /> */}
+            <img src="/images/Card5.png" alt="" className="rounded-2xl pb-6 w-full max-w-lg object-cover" />
+            <img src="/images/Card6.png" alt="" className="rounded-2xl w-full max-w-lg object-cover" />
           </div>
         </div>
       </div>
 
-      <section className="bg-[#4B00FF] text-white text-center py-24 rounded-2xl my-10 mx-4 md:mx-[360px]">
-        <h2 className="text-2xl md:text-3xl font-semibold mb-6">
+
+      <section className="flex flex-col items-center justify-center h-[300px] text-center mx-4 my-10 md:mx-[235px] md:my-20 rounded-2xl text-white bg-blue-700">
+        <h2 className="text-3xl md:text-4xl font-semibold mb-4">
           Enterprise? <br /> Get your mirror.
         </h2>
-        <button className="bg-white text-black text-sm font-medium px-5 py-2 rounded-md hover:bg-gray-200 transition-all duration-300">
+        <button className="bg-white text-black font-medium px-6 py-2 rounded-md hover:bg-gray-200 transition-all duration-200">
           Learn more
         </button>
       </section>
